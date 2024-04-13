aa16718f0a76c991171632372b1b8da72b58b1b8
Iam-Jorge <Angelovg7498@gmail.com>
2024-04-13 16:34:32 +0200

    Modelos

--- Backend/models/Grado.js
+++ Backend/models/Grado.js
@@ -1,12 +1,12 @@
-  const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-  const gradoSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    asignaturas: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Asignatura'
-    }]
-    // Puedes añadir más campos aquí según sea necesario
-  });
+const gradoSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  asignaturas: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Asignatura'
+  }]
+  // Puedes añadir más campos aquí según sea necesario
+});
 
-  module.exports = mongoose.model('Grado', gradoSchema);
+module.exports = mongoose.model('Grado', gradoSchema);
